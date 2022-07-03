e2ebd20b13630774f1ee47b398e3dc5c61325468
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-07-03 11:03:24 -1100

    crear roles automaticos

--- src/main/java/com/example/email/seeders/Usuario.java
+++ src/main/java/com/example/email/seeders/Usuario.java
@@ -4,18 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.email.model.Rol;
 import com.example.email.model.User;
+import com.example.email.repository.RolRepository;
 import com.example.email.repository.UserRepository;
 
 @Component
 public class Usuario implements CommandLineRunner{
 
 	@Autowired
-	private UserRepository userRepository;		
+	private UserRepository userRepository;
+	@Autowired
+	private RolRepository rolRepo;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		cargarUser();
-		
+		crearRoles();
 	}	
 	private void cargarUser() {			
 		if(userRepository.count()==0) {
@@ -30,5 +35,22 @@ public class Usuario implements CommandLineRunner{
 		User user =new User(nombre, password);
 		userRepository.save(user);
 	}
+	
+	
+	
+	private void crearRoles() {			
+		if(rolRepo.count()==0) {
+			
+			Rol rolAdmin =new Rol("ROL_ADMIN");
+			Rol rolUser=new Rol("ROL_USER");
+			rolRepo.save(rolAdmin);
+			rolRepo.save(rolUser);
+			
+		}
+	}
+	
+	
+	
+	
 
 }
